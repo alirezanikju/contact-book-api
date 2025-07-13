724cfd5e42642adb9369f8f0722f78c7221e3095
alireza nikju <alirezanikjoo2018@gmail.com>
2025-07-13 19:57:26 +0330

    add data with post method on json file

--- api/contacts.php
+++ api/contacts.php
@@ -2,25 +2,62 @@
 
 header("Content-Type: application/json");
 
-if ($_SERVER["REQUEST_METHOD"] !== "GET"){
+if($_SERVER["REQUEST_METHOD"] === "POST"){
+
+    $rawData = file_get_contents("php://input");
+    echo $rawData;
+    $data = json_decode($rawData , true);
+
+    if(!isset($data['name']) || !isset($data['phone'])){
+        echo json_encode([
+            "status"=> "error",
+            "msg"=> "add name and phone number"
+        ], JSON_UNESCAPED_UNICODE);
+    exit;
+    }
+
+    $contacts = json_decode(file_get_contents("../contacts.json"), true);
+
+
+    if(!is_array($contacts)){
+        $contacts = [];
+    }
+
+    $newContacts = [
+        "id"=> uniqid(),
+        "name"=> $data['name'],
+        "phone"=> $data['phone']
+    ];
+
+    $contacts[] = $newContacts;
+
+    file_put_contents("../contacts.json" , json_encode($contacts, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE));
+
     echo json_encode([
-        "status"=> "error",
-        "msg"=> "just get method"
-    ], JSON_UNESCAPED_UNICODE);
+        "status"=>"ok",
+        "contacts"=>$newContacts
+    ]);
+
 }
+elseif($_SERVER["REQUEST_METHOD"] === "GET"){
+    // echo json_encode([
+    //     "status"=> "error",
+    //     "msg"=> "just get method"
+    // ], JSON_UNESCAPED_UNICODE);
+// }
 
-$contects = json_decode(file_get_contents("../contacts.json"), true);
+$contacts = json_decode(file_get_contents("../contacts.json"), true);
 
-if(!is_array($contects)){
-    $contects = [];
+if(!is_array($contacts)){
+    $contacts = [];
 }
 
 
 echo json_encode([
     "status"=> "ok",
-    "contects"=> $contects
+    "contacts"=> $contacts
 ], JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
 
-
+}
 
 
